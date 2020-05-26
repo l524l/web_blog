e5a7531d42925194ba26c4812c788a96da046e33
l524l <65085630+l524l@users.noreply.github.com>
2020-05-26 15:51:49 +0300

    Initial commit

---
+++ src/test/java/com/l524l/web_blog/WebBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l524l.web_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
