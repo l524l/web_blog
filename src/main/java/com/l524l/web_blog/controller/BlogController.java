e88063e60c6e0a7cf0668466567352ada647258a
l524l <65085630+l524l@users.noreply.github.com>
2020-05-27 13:33:50 +0300

    создана полная функциональность блога

--- src/main/java/com/l524l/web_blog/controller/BlogController.java
+++ src/main/java/com/l524l/web_blog/controller/BlogController.java
@@ -6,6 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -18,4 +25,61 @@ public class BlogController {
         model.addAttribute("posts", posts);
         return "blog_page";
     }
+    @GetMapping("/blog/add")
+    public String addPostPage(Model model){
+        return "add_page";
+    }
+    @PostMapping("/blog/add")
+    public String addPost(@RequestParam(name = "title") String title,
+                          @RequestParam(name = "anons") String anons,
+                          @RequestParam(name = "full_text") String full_text,
+                          Model model){
+
+        Post post = new Post(title,anons,full_text);
+        postRepository.save(post);
+        return "add_page";
+    }
+    @GetMapping("/blog/post{ID}")
+    public String blogMoreInfo(@PathVariable(value = "ID") long ID, Model model){
+        Optional<Post> post = postRepository.findById(ID);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post",res);
+
+        return "post_page";
+    }
+    @GetMapping("/blog/post{ID}/edit")
+    public String blogEdit(@PathVariable(value = "ID") long ID, Model model){
+        Optional<Post> post = postRepository.findById(ID);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post",res);
+
+        return "post_edit";
+    }
+    @PostMapping("/blog/post{ID}/edit")
+    public String editPost(@PathVariable(value = "ID") long ID,
+                           @RequestParam(name = "title") String title,
+                           @RequestParam(name = "anons") String anons,
+                           @RequestParam(name = "full_text") String full_text,
+                          Model model){
+
+        Post post = postRepository.findById(ID).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+
+        return "redirect:/blog";
+    }
+    @PostMapping("/blog/post{ID}/delete")
+    public String editPost(@PathVariable(value = "ID") long ID, Model model){
+        Post post = postRepository.findById(ID).orElseThrow();
+        postRepository.delete(post);
+
+        return "redirect:/blog";
+    }
+
+
+
 }
--- src/main/java/com/l524l/web_blog/controller/MainController.java
+++ src/main/java/com/l524l/web_blog/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.l524l.web_blog.controller;
 
+import com.l524l.web_blog.models.Post;
+import com.l524l.web_blog.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +10,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-
+    @Autowired
+    private PostRepository postRepository;
     @GetMapping("/")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="blog") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "main_page";
     }
 
