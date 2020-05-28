2fd2cb659f16958dd5f02d691d8ec970120fd14b
l524l <65085630+l524l@users.noreply.github.com>
2020-05-28 20:08:15 +0300

    тестовая работа с beans

---
+++ src/main/java/com/l524l/web_blog/Test.java
@@ -0,0 +1,7 @@
+package com.l524l.web_blog;
+
+public class Test {
+    public String helloWorl(){
+        return "hello world";
+    }
+}
--- src/main/java/com/l524l/web_blog/WebBlogApplication.java
+++ src/main/java/com/l524l/web_blog/WebBlogApplication.java
@@ -2,11 +2,19 @@ package com.l524l.web_blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class WebBlogApplication {
 
 	public static void main(String[] args) {
+
+		ApplicationContext ctx = new
+				ClassPathXmlApplicationContext(WebBlogApplication.class.getClassLoader().getResource("beans.xml").toExternalForm());
+		System.out.println(ctx.getBean("test",Test.class).helloWorl());
+		((ClassPathXmlApplicationContext) ctx).close();
+
 		SpringApplication.run(WebBlogApplication.class, args);
 	}
 
