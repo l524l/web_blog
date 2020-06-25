96493efa4f9cb814dd659635ca1b1d8bbd423ab8
l524l <65085630+l524l@users.noreply.github.com>
2020-06-25 11:16:59 +0300

    Нововведения: добавлен BCrypt

--- src/main/java/com/l524l/web_blog/config/WebSecurityConfig.java
+++ src/main/java/com/l524l/web_blog/config/WebSecurityConfig.java
@@ -3,12 +3,14 @@ package com.l524l.web_blog.config;
 import com.l524l.web_blog.service.UserService;
 import com.l524l.web_blog.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,6 +21,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserServiceImpl userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder(8);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,7 +47,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder passwordEncoder = NoOpPasswordEncoder.getInstance();
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
     }
 }
