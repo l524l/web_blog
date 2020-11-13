a82f3a22522e52849e0c08706f7cf712d068ebc4
l524l <65085630+l524l@users.noreply.github.com>
2020-11-13 17:22:06 +0300

    Убраны пробелы

--- src/main/java/com/l524l/web_blog/config/MailConfig.java
+++ src/main/java/com/l524l/web_blog/config/MailConfig.java
@@ -20,7 +20,6 @@ public class MailConfig {
     private String protocol;
     @Value("${mail.debug}")
     private String debug;
-
     @Bean
     public JavaMailSender getMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
